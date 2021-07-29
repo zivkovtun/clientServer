0d83c09953ccc6fc66e494f56f581637eba799d4
Gal <galeini6@gmail.com>
2021-07-29 19:07:43 +0300

    some init msg

---
+++ PortScanner/PortScanner/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PortScanner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
